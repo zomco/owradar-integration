9b9fb476847101bf83f635c0993cfddc81aeab82
zomco <zomcoo@gmail.com>
2023-11-07 11:01:28 +0800

    Initial commit

---
+++ custom_components/owr_care/const.py
@@ -0,0 +1,9 @@
+"""Constants for owr_care."""
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+NAME = "owr care"
+DOMAIN = "owr_care"
+VERSION = "0.0.0"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
